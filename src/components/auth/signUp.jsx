1a23b74c333f070f90bd676e54d9a8cd974032ee
abhijithes6 <abhijithes266.com>
2025-08-08 19:13:54 +0530

    Fix headers

--- src/components/auth/signUp.jsx
+++ src/components/auth/signUp.jsx
@@ -3,75 +3,66 @@ import "./auth.css";
 import { useNavigate } from "react-router-dom";
 
 export default function SignUp({ handleAuthType }) {
-  const API_URL = "https://my-ecomm-json-server.onrender.com";
-  const [userData, setUserData] = useState({});
-  const navigate = useNavigate();
-  const handleChange = (event) => {
-    setUserData((prev) => ({
-      ...prev,
-      [event.target.name]: event.target.value,
-    }));
-  };
-  const onSubmit = async (event) => {
-    console.log(userData)
-    event.preventDefault();
-    if (userData.password !== userData.Confirmpassword) {
-      alert("Passwords do not match!");
-      return;
-    }
-    try {
-      const res = await fetch(`${API_URL}/users`, {
-        method: "POST",
-        body: JSON.stringify(userData),
-      });
-      if(res.ok){
-       alert("account created Successfully!")
-       localStorage.setItem('user',JSON.stringify(userData));
-       navigate('/products') 
-       window.location.reload();
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
+    const API_URL = "https://my-ecomm-json-server.onrender.com";
+    const [userData, setUserData] = useState({});
+    const navigate = useNavigate();
+    const handleChange = (event) => {
+        setUserData((prev) => ({
+            ...prev,
+            [event.target.name]: event.target.value,
+        }));
+    };
+    const onSubmit = async (event) => {
+        console.log(userData);
+        event.preventDefault();
+        if (userData.password !== userData.Confirmpassword) {
+            alert("Passwords do not match!");
+            return;
+        }
+        try {
+            const res = await fetch(`${API_URL}/users`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(userData),
+            });
+            if (res.ok) {
+                alert("account created Successfully!");
+                localStorage.setItem("user", JSON.stringify(userData));
+                navigate("/products");
+                window.location.reload();
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
-  return (
-    <div className="sign-up-in">
-      <p>Create a new account</p>
-      <form action="" method="POST">
-        <input
-          className="component"
-          onChange={handleChange}
-          type="text"
-          name="name"
-          placeholder="Name"
-        />
-        <input
-          className="component"
-          onChange={handleChange}
-          type="email"
-          name="email"
-          placeholder="E-Mail"
-        />
-        <input
-          className="component"
-          onChange={handleChange}
-          type="password"
-          name="password"
-          placeholder="Password"
-        />
-        <input
-          className="component"
-          type="password"
-          name="Confirmpassword"
-          onChange={handleChange}
-          placeholder="Confirm password"
-        />
-      </form>
-      <button onClick={onSubmit}>Create Account</button>
-      <p>
-        Already have an account? <a onClick={handleAuthType}>Login</a>
-      </p>
-    </div>
-  );
+    return (
+        <div className="sign-up-in">
+            <p>Create a new account</p>
+            <form action="" method="POST">
+                <input className="component" onChange={handleChange} type="text" name="name" placeholder="Name" />
+                <input className="component" onChange={handleChange} type="email" name="email" placeholder="E-Mail" />
+                <input
+                    className="component"
+                    onChange={handleChange}
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                />
+                <input
+                    className="component"
+                    type="password"
+                    name="Confirmpassword"
+                    onChange={handleChange}
+                    placeholder="Confirm password"
+                />
+            </form>
+            <button onClick={onSubmit}>Create Account</button>
+            <p>
+                Already have an account? <a onClick={handleAuthType}>Login</a>
+            </p>
+        </div>
+    );
 }
